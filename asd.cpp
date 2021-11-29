ef7b97b302dd218d3588e8a03268d6e210fa6d08
khj94811 <khj94811@naver.com>
2021-11-29 20:02:42 +0900

    Update asd.cpp
    Signed-off-by: khj94811 <khj94811@naver.com>

--- asd.cpp
+++ asd.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(){
     cout << "Hello World" << endl;
-
+    for (int i = 0; i < 10; i++) {
+        cout << "I don't like hello world project bro.";
+    }
     return 0;
 }
